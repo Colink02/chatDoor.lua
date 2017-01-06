53c5c5e5c5fba40f1372df3d31b07013051fbfa0
colin500 <colinkinzel@gmail.com>
2017-01-06 08:57:26 -0600

    Update PassDoor.lua

--- PassDoor.lua
+++ PassDoor.lua
@@ -10,6 +10,7 @@ local delay = 3
 --------
 while on do
 local _,_,message = event.pull(chat_message)
+  
 if message = "Open Door" then
   rs.setOutput(side, 15)
   os.sleep(delay)
@@ -17,5 +18,5 @@ if message = "Open Door" then
   end
 if message = "end" then
     on = false
-    print("0000000000000000000000000000000")
+    print("Good Bye")
 end
